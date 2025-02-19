9e3fb66e1fe9c9edb76a266ad4d2763f9abfc68f
Alex Siega <alemosie@users.noreply.github.com>
2025-02-19 10:32:07 +0100

    Start swapping template assets and settings with personal data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,9 +7,10 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/amsterdamparentproject/site',
-  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo/dark.png`,
+  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/web-share.png`,
+  mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
   github: 'https://github.com',
   linkedin: 'https://www.linkedin.com',
