54a02e00576d9f5f1d3541f7b7f471e6827cf754
Alex Siega <alemosie@users.noreply.github.com>
2025-02-20 12:37:46 +0100

    Add /disclaimer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,18 @@
 import Link from "./Link";
 import siteMetadata from "@/data/siteMetadata";
 import SocialIcon from "@/components/social-icons";
+import NewsletterForm from "pliny/ui/NewsletterForm";
 
 export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
-        <div className="mb-3 flex space-x-4">
+        {siteMetadata.newsletter?.provider && (
+          <div className="flex items-center justify-center pt-4 mb-6">
+            <NewsletterForm />
+          </div>
+        )}
+        <div className="mb-4 flex space-x-4">
           <SocialIcon
             kind="mail"
             href={`mailto:${siteMetadata.email}`}
@@ -16,10 +22,12 @@ export default function Footer() {
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
           <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
         </div>
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="mb-4 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <Link href="/">{siteMetadata.title}</Link>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{` • `}</div>
+          <Link href="/disclaimer">Disclaimer</Link>
         </div>
       </div>
     </footer>
