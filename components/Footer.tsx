b40041c4d8e32171b736b8b5a33131b71ac80b5c
Alex Siega <alemosie@users.noreply.github.com>
2025-02-22 20:43:47 +0100

    Create custom subscribe input

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,42 +1,36 @@
 import Link from "./Link";
 import siteMetadata from "@/data/siteMetadata";
-import NewsletterForm from "pliny/ui/NewsletterForm";
 import ThemeSwitch from "./ThemeSwitch";
+import NewsletterSubscribe from "./NewsletterSubscribe";
 
 export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
-        <div>
-          <Link href="/subscribe" aria-label="Subscribe">
-            <p className="font-medium text-brand-charcoal hover:text-brand-soft-green dark:text-brand-white dark:hover:text-brand-goldenrod">
-              Subscribe to our newsletter:
-            </p>
-          </Link>
-          {siteMetadata.newsletter?.provider && (
-            <div className="flex items-center justify-center mt-1 mb-6">
-              <iframe
-                title="newsletter-subscribe-footer"
-                src="https://embeds.beehiiv.com/91a659aa-6a60-4d9e-9b7a-03f3f1e5f98e?slim=true"
-                data-test-id="beehiiv-embed"
-                height="52"
-                className="bg-transparent"
-                suppressHydrationWarning
-              ></iframe>
-            </div>
-          )}
+        <div className="mb-4">
+          <NewsletterSubscribe />
         </div>
 
-        <div className="mb-2 flex space-x-2 text-sm text-brand-soft-charcoal dark:text-brand-white">
+        <div className="mb-2 flex space-x-2 text-sm">
           <ThemeSwitch />
         </div>
 
         <div className="mb-4 flex space-x-2 text-sm text-brand-soft-charcoal dark:text-brand-white">
-          <Link href="/">{siteMetadata.title}</Link>
+          <Link
+            href="/"
+            className="hover:text-brand-soft-green dark:hover:text-brand-goldenrod"
+          >
+            {siteMetadata.title}
+          </Link>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
-          <Link href="/disclaimer">Disclaimer</Link>
+          <Link
+            href="/disclaimer"
+            className="hover:text-brand-soft-green dark:hover:text-brand-goldenrod"
+          >
+            Disclaimer
+          </Link>
         </div>
       </div>
     </footer>
---
+++ components/NewsletterSubscribe.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import siteMetadata from "@/data/siteMetadata";
+import { useState } from "react";
+
+const NewsletterSubscribe = (Props) => {
+  const { label } = Props;
+  const [emailInput, setEmailInput] = useState("");
+
+  const updateEmailInput = (e) => {
+    const val = e.target.value;
+    setEmailInput(val);
+  };
+
+  const subscribe = (e) => {
+    e.preventDefault();
+
+    console.log(`Subscribed! ${emailInput} ${window.location}`);
+
+    setEmailInput("");
+  };
+
+  if (siteMetadata.newsletter?.provider) {
+    return (
+      <form className="max-w-sm mx-auto">
+        <label
+          htmlFor="subscribe-email"
+          className="block mb-2 font-medium text-brand-charcoal dark:text-brand-white"
+        >
+          {label ? label : "Subscribe to our newsletter:"}
+        </label>
+        <div className="flex">
+          <input
+            type="text"
+            id="subscribe-email"
+            className="rounded-none rounded-l-lg bg-white border border-2 border-brand-goldenrod text-brand-charcoal 
+                block flex-1 min-w-0 w-full 
+                placeholder-brand-soft-charcoal/60
+                text-md p-2.5 focus:ring-transparent"
+            onChange={updateEmailInput}
+            value={emailInput}
+            placeholder="Enter your email"
+          />
+          <button
+            className="inline-flex cursor-pointer items-center px-3 text-md text-brand-charcoal bg-brand-goldenrod
+                border border-l-0 border-brand-goldenrod rounded-e-md"
+            onClick={subscribe}
+            type="submit"
+          >
+            Subscribe
+          </button>
+        </div>
+      </form>
+    );
+  }
+};
+
+export default NewsletterSubscribe;
